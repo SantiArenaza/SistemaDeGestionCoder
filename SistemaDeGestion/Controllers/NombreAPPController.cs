5c0a7aeb216f5f696760dd8648278dc06173d26d
santi <santi@PC-PRINCIPAL>
2022-12-17 14:09:55 -0300

    Se agrega "Traer Nombre"

---
+++ SistemaDeGestion/Controllers/NombreAPPController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeGestion.Modelos;
+using SistemaDeGestion.Repositorios;
+
+namespace SistemaDeGestion.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NombreAPPController : Controller
+    {
+
+        private NombreAppRepositorio repositorio = new NombreAppRepositorio();
+
+        [HttpGet]
+        public IActionResult Get()  //accion de consulta
+        {
+            try
+            {
+                string nombreApp = repositorio.nombreApp(); //llamo a la funcion listarproductos dentro de la clase producto repositorio y guardo la lista
+                return Ok(nombreApp); //retorno la lista
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
---
+++ SistemaDeGestion/Repositorios/NombreAppRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using SistemaDeGestion.Modelos;
+using System.Data.SqlClient;
+
+namespace SistemaDeGestion.Repositorios
+{
+    public class NombreAppRepositorio
+    {
+        //defino la cadena de conexion
+        private SqlConnection? conexion;
+        private String cadenaconexion = "Server=sql.bsite.net\\MSSQL2016;" +
+                "Database=santiarenaza_primer_db;" +
+                "User Id=santiarenaza_primer_db;" +
+                "Password=123456;";
+
+        public NombreAppRepositorio()
+        {
+            try
+            {
+                conexion = new SqlConnection(cadenaconexion);
+            }
+            catch
+            {
+
+            }
+        }
+
+        public string nombreApp()
+        {
+            return ("MiApp");
+        }
+
+
+
+    }
+}
